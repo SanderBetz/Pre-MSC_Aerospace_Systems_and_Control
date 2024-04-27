f0c3eddb0bc42a6561f2693c05f6844b06e3574a
SanderBetz <sander.betz@me.com>
2024-04-27 18:16:37 +0200

    Initial commit and push for project

--- Week 1/Problem 1.py
+++ Week 1/Problem 1.py
@@ -1,9 +1,14 @@
-# Put imports here
+import numpy as np
+np.random.seed(7)
 
-# Main code
-def main():
-    print('Hello, world!')
+mat = np.random.randn(5, 5)
+mat = np.array([[int(round(i, 0)) for i in mat[j]] for j, value in enumerate(mat)])
+rowvector = np.arange(-2,3).reshape((1,5))
+columnvector = np.arange(0,6).reshape((6, 1))
 
+mat = np.vstack([mat, rowvector])
+mat = np.hstack([mat, columnvector])
+non_zeros = np.count_nonzero(mat)
 
-if __name__ == '__main__':
-    main()
+print(non_zeros)
+
